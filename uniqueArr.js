f4c4160fec14076403a82e2ca233384aa85ecdcc
gesangs <gesangs@qq.com>
2018-03-30 10:22:14 +0800

    fix

--- uniqueArr.js
+++ uniqueArr.js
@@ -10,13 +10,13 @@ const unique = (arr) => [...new Set(arr)]
 // index：当前值的索引
 // a：    当前值所属的数组
 // []：   运算初始值
-
+// Map结构的key值可以是任意类型，而普通对象的key值是字符串类型，无法区分字符‘1’和数字1
 
  const quchong = (arr) => {
-      const hash = {};
-        arr = arr.reduce(function (item, value, index, a) {
-          hash[value] ? '' : hash[value] = true && item.push(value);
+      let hash = new Map();
+      arr = arr.reduce((item, value) => {
+          hash.has(value) ? '' : hash.set(value, true) && item.push(value)
           return item;
-        }, []);
-        return arr;
+      }, [])
+      return arr;
     };
